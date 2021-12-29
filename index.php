bcf37b2e1fd17785a3d4dc32acf88e296a6f81f9
Faisalsaleh2202 <faisal.saleh127@gmail.com>
2021-12-29 10:08:03 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis et quidem exercitationem incidunt voluptas omnis libero fugit similique? Quam nesciunt consequatur debitis, unde vel amet ad. Soluta eligendi excepturi porro.</p>
+</body>
+</html>
